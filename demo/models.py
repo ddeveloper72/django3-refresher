ab753ac1790d7ca09bb8d2d1a36173fc5e0d2997
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-20 23:00:10 +0100

    completed Book model with assistance of make migrations error messages

--- demo/models.py
+++ demo/models.py
@@ -7,11 +7,17 @@ class Book(models.Model):
     title = models.CharField(blank=False,
                              unique=True,
                              max_length=36)
-    description = models.TextField(max_length=256, blank=True)
+    description = models.TextField(max_length=256,
+                                   blank=True)
 
-    price = models.DecimalField(default=0, decimal_places=2)
+    price = models.DecimalField(default=0,
+                                max_digits=3,
+                                decimal_places=2)
 
-    published = models.DateField()
+    published = models.DateField(blank=True,
+                                 null=True,
+                                 default=None)
     is_published = models.BooleanField(default=False)
 
-    cover = models.ImageField(upload_to='covers/')
+    cover = models.ImageField(upload_to='covers/',
+                              blank=True)
