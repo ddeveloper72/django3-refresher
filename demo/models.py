79a41557dc44b292337cdf0fb0110a13c897fff9
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-20 22:17:57 +0100

    first commit

---
+++ demo/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Book(models.Model):
+    title = models.CharField(blank=False,
+                             unique=True,
+                             max_length=36)
