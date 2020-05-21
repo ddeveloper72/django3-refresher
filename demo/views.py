fdea4fba169adf46639550c51bb002ab1fe9a867
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-21 23:14:55 +0100

    added all books for DB to view

--- demo/views.py
+++ demo/views.py
@@ -8,4 +8,6 @@ from django.shortcuts import render
 
 
 def first(request):
-    return render(request, 'first_temp.html', {'data': 'this is data from the first view'})
+    books = Book.objects.all()
+
+    return render(request, 'first_temp.html', {'books': books})
