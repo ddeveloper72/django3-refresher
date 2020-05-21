066436d5f8f824f4e16d10f866d4e465fa32a509
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-21 23:08:28 +0100

    refactored viw to render a reqeust to a template file

--- demo/views.py
+++ demo/views.py
@@ -2,23 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
 from .models import Book
+from django.shortcuts import render
 
 # Create your views here.
 
 
-class Another(View):
-
-    book = Book.objects.get(id=1)
-    # setup empty string as output
-    output = f"We have {book.title}  with ID {book.id}<br>"
-    # for book in books:
-
-        # concatenate the strings together else last line overwrites 1st
-        # output += f"We have {book.title}  with ID {book.id}<br>"
-
-    def get(self, request):
-        return HttpResponse(self.output)
-
-
 def first(request):
-    return HttpResponse('First message from views')
+    return render(request, 'first_temp.html')
