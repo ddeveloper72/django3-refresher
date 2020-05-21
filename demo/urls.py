d2482841d9684113bd2e59c30c65335b53ff1ba7
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-21 22:21:49 +0100

    added url pattern for call called Another

--- demo/urls.py
+++ demo/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import Another
 
 urlpatterns = [
-    path('firstfunction', views.first),
+    path('first', views.first),
+    path('another', Another.as_view()),
 ]
