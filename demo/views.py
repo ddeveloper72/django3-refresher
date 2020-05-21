ea1dead8cfa84080161bab4077bcd5e3e997c593
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-21 22:34:01 +0100

    import Book model and return no. books in DB

--- demo/views.py
+++ demo/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .models import Book
 
 # Create your views here.
 
 
 class Another(View):
 
+    books = Book.objects.all()
+
+    output = f"We have {len(books)}  books in our DB"
+
     def get(self, request):
-        return HttpResponse('This is another function from inside the class')
+        return HttpResponse(self.output)
 
 
 def first(request):
