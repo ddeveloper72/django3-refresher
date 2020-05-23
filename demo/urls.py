c9609a2f985729f1c70bc479192d10f3598c41cd
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-23 10:33:02 +0100

    crate a new variable called router

--- demo/urls.py
+++ demo/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 from rest_framework import routers
 
+#  crate a new variable called router
+router = routers.DefaultRouter()
+
 urlpatterns = [
     path('first', views.first),
 ]
