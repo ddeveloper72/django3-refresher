b4a4983bc6bccedbf49845f51416b55a491c122b
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-23 18:49:28 +0100

    added the cCharacter model to the BookSerializer

--- demo/serializers.py
+++ demo/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Book, BookNumber
+from .models import Book, BookNumber, Character
 
 
 class BookNumberSerializer(serializers.ModelSerializer):
@@ -8,10 +8,16 @@ class BookNumberSerializer(serializers.ModelSerializer):
         # specify the fields available from serializer
         fields = ['id', 'isbn_10', 'isbn_13']
 
+class CharacterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Character
+        # specify the fields available from serializer
+        fields = ['id', 'name']
 
 class BookSerializer(serializers.ModelSerializer):
     number = BookNumberSerializer(many=False)
+    characters = CharacterSerializer(many=True)
     class Meta:
         model = Book
         # specify the fields available from serializer
-        fields = ['id', 'title', 'description', 'price', 'published', 'is_published', 'number']
+        fields = ['id', 'title', 'description', 'price', 'published', 'is_published', 'number', 'characters']
