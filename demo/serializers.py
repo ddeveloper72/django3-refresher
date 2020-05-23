1f7320d867fc6a49e513e0528a3c3507d740bbe0
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-23 18:22:27 +0100

    added a new class called BookNumberSerializer & mapped to BookSerializer

--- demo/serializers.py
+++ demo/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
-from .models import Book
+from .models import Book, BookNumber
 
 
-class BookSerializer(serializers.ModelSerializer):
+class BookNumberSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         # specify the fields available from serializer
-        fields = ['id', 'title', 'description', 'price', 'published', 'is_published']
+        fields = ['id', 'isbn_10', 'isbn_13',]
+
+
+class BookSerializer(serializers.ModelSerializer):
+    number = BookNumberSerializer(many=False)
+    class Meta:
+        model = Book
+        # specify the fields available from serializer
+        fields = ['id', 'title', 'description', 'price', 'published', 'is_published', 'number']
