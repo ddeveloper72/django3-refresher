c9b50ffab28b7beae7b91858b726ead247676421
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-23 09:30:09 +0100

    make specific fields available to book object in admin

--- demo/admin.py
+++ demo/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Book
 
 # Register your models here.
-admin.site.register(Book)
+
+# make specific fields available to book object in admin
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    fields = ['title', 'description']
