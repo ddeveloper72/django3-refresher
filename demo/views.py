ccc342718128fe5f710085ebcda19683cc02333e
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-23 10:07:23 +0100

    added class from defined in serializer.py

--- demo/views.py
+++ demo/views.py
@@ -14,3 +14,4 @@ def first(request):
     return render(request, 'first_temp.html', {'books': books})
 # crate a builtin view using viewsets
 class BookViewSet(viewsets.MoedelViewSet):
+    serializer_class = BookSerializer # from class defined in serializer.py
