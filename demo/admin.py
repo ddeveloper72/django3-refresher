3dcc3ecc1359665f93226038b8f4ada435d1dcff
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-23 09:34:22 +0100

    using fields or list_display

--- demo/admin.py
+++ demo/admin.py
@@ -4,6 +4,8 @@ from .models import Book
 # Register your models here.
 
 # make specific fields available to book object in admin
+# using fields or list_display
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    fields = ['title', 'description']
+    # fields = ['title', 'description']
+    list_display = ['title', 'title', 'price']
