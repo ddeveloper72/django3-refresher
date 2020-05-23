fdd1b2ed85ce70fd868884efcf27980e0d84794a
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-23 10:35:03 +0100

    crate router called books & pass the viewset from the views

--- demo/urls.py
+++ demo/urls.py
@@ -4,6 +4,8 @@ from rest_framework import routers
 
 #  crate a new variable called router
 router = routers.DefaultRouter()
+# crate router called books & pass the viewset from the views
+router.register('books', BookViewSet)
 
 urlpatterns = [
     path('first', views.first),
