87ccfb5ffea89af9c91d5aabc1ed2a24a1aa0f17
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-23 14:54:28 +0100

    added auth path for aobtain auth token

--- first/urls.py
+++ first/urls.py
@@ -20,5 +20,5 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('demo/', include('demo.urls')),
     path('admin/', admin.site.urls),
-    path('auth/')
+    path('auth/', obtain_auth_token)
 ]
