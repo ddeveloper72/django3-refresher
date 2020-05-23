79798b347f8f228117b612b084283b2008d83d6f
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-23 09:41:23 +0100

    adding more parametes to search_fields filter

--- demo/admin.py
+++ demo/admin.py
@@ -7,9 +7,10 @@ from .models import Book
 # using fields or list_display
 # adding a list_filter filter
 # adding a search_fields filter
+# adding more parametes to search_fields filter
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     # fields = ['title', 'description']
     list_display = ['title', 'title', 'price']
     list_filter = ['published']
-    search_fields = ['title']
+    search_fields = ['title', 'description']
