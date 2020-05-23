363fd0dea788dac5527f9e961e3e2ae09c76bb3f
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-23 18:20:04 +0100

    added BookNumber to admin site from Model

--- demo/admin.py
+++ demo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, BookNumber
 
 # Register your models here.
 
@@ -14,3 +14,6 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'title', 'price']
     list_filter = ['published']
     search_fields = ['title', 'description']
+
+
+admin.site.register(BookNumber)
