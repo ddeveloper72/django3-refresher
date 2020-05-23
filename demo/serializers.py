a41bb429731737c957d13d2da6e753d4beb90377
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-23 14:18:01 +0100

    specify the fields available from serializer

--- demo/serializers.py
+++ demo/serializers.py
@@ -5,4 +5,5 @@ from .models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ['title']
+        # specify the fields available from serializer
+        fields = ['title', 'description', 'price']
