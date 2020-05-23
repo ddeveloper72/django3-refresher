6932d0f40a07107ead765181d71e360887ef52d9
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-23 10:12:48 +0100

    removed all unused code

--- demo/views.py
+++ demo/views.py
@@ -1,18 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views import View
-from .models import Book
-from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import BookSerializer
 
 # Create your views here.
 
 
-def first(request):
-    books = Book.objects.all()
-
-    return render(request, 'first_temp.html', {'books': books})
 # crate a builtin view using viewsets
-class BookViewSet(viewsets.MoedelViewSet):
-    serializer_class = BookSerializer # from class defined in serializer.py
+class BookViewSet(viewsets.ModelViewSet):
+    serializer_class = BookSerializer  # from class defined in serializer.py
