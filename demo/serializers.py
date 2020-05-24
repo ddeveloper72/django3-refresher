7f2baaa50e9e50e1060464ba9bea0aebed3cfda8
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-24 09:16:57 +0100

    added Authors to serializer

--- demo/serializers.py
+++ demo/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Book, BookNumber, Character
+from .models import Book, BookNumber, Character, Author
 
 
 class BookNumberSerializer(serializers.ModelSerializer):
@@ -14,10 +14,21 @@ class CharacterSerializer(serializers.ModelSerializer):
         # specify the fields available from serializer
         fields = ['id', 'name']
 
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        # specify the fields available from serializer
+        fields = ['id', 'name', 'surname']
+
+
 class BookSerializer(serializers.ModelSerializer):
     number = BookNumberSerializer(many=False)
     characters = CharacterSerializer(many=True)
+    authors = AuthorSerializer(many=True)
+    
     class Meta:
         model = Book
         # specify the fields available from serializer
-        fields = ['id', 'title', 'description', 'price', 'published', 'is_published', 'number', 'characters']
+        fields = ['id', 'title', 'description',
+                  'price', 'published', 'is_published',
+                  'number', 'characters', 'authors']
